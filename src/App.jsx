a65c684a6d52ef1510d87c3c996cb74f87c9dab3
RudrakshaSingh <rudrakshasingh587@gmail.com>
2023-09-28 03:17:10 -0700

    todo with context and reducer format

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
-import { useState } from "react";
+import { useReducer } from "react";
 import TodoContext from "./context/TodoContext";
 import AddTodo from "./components/AddTodo/AddTodo";
 import TodoList from "./components/TodoList/TodoList";
+import todoReducer from "./reducers/todoReducer";
+import TodoDispatchContext from "./context/TodoDispatchContext";
 
 function App() {
-    const [list, setList] = useState([
-        { id: 1, todoData: "todo 1", finished: false },
-        { id: 2, todoData: "todo 2", finished: true },
-    ]);
+    const [list, dispatch] = useReducer(todoReducer, []);
 
     return (
-        <TodoContext.Provider value={{ list, setList }}>
-            <AddTodo
-                updateList={(todo) =>
-                    setList([...list, { id: list.length + 1, todoData: todo, finished: false }])
-                }
-            />
-            <TodoList list={list} updateList={setList} />
+        <TodoContext.Provider value={{ list }}>
+            <TodoDispatchContext.Provider value={{ dispatch }}>
+                <AddTodo />
+                <TodoList />
+            </TodoDispatchContext.Provider>
         </TodoContext.Provider>
     );
 }
